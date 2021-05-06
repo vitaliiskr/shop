5c078601e729e2ebd5eb9a1a56e3a53d433f90ec
Vitalii Skrynkovych <vitaliiskrynkovych@gmail.com>
2021-05-06 14:09:07 +0300

    without LocalStorage

--- src/products.js
+++ src/products.js
@@ -12,14 +12,14 @@ const products = [
     name: "article 2",
     label: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: 35,
-    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1e4eabd1-bc42-48bf-b3fd-6c535a4f6b6f/sb-zoom-blazer-low-pro-gt-skate-shoe-bMwZmc.jpg",
+    img: "https://github.com/vitaliiskr/shop/blob/master/src/images/product-1.jpg?raw=true",
   },
   {
     id: 3,
     name: "article 3",
     label: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: 45,
-    img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5a532d94-5435-4598-bac8-128302d25789/blazer-mid-77-suede-shoe-HxFGDP.png",
+    img: "https://github.com/vitaliiskr/shop/blob/master/src/images/product-2.jpg?raw=true",
   },
 ];
 
